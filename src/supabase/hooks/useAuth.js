351c38474c25e67e06c6f5bad0c052cc07ede605
Sean <>
2022-03-05 14:08:32 -0800

    amazing template

--- src/supabase/hooks/useAuth.js
+++ src/supabase/hooks/useAuth.js
@@ -2,10 +2,7 @@ import { useEffect, useState, createContext, useContext } from 'react';
 import { useRouter } from 'next/router';
 import { supabase } from '../initSupabase';
 
-// export const SignOut = async () => {
-//   await supabase.auth.signOut();
-// };
-
+//* below is from here: https://github.com/supabase/supabase/blob/master/examples/nextjs-auth-tailwind/pages/auth.js
 export const RequireAuth = () => {
   const { user } = useAuth();
   const router = useRouter();
@@ -16,46 +13,6 @@ export const RequireAuth = () => {
     }
   }, [user, router]);
 };
-
-// export const AuthRedirect = () => {
-//   const { user } = useUser();
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (user) {
-//       router.push('/profile');
-//     }
-//   }, [user, router]);
-// };
-
-// export const UserContext = createContext();
-
-// export const UserContextProvider = (props) => {
-//   const [session, setSession] = useState(false);
-//   const [user, setUser] = useState(false);
-
-//   useEffect(() => {
-//     const session = supabase.auth.session();
-//     setSession(session);
-//     setUser(session?.user ?? false);
-//     const { data: authListener } = supabase.auth.onAuthStateChange(
-//       async (event, session) => {
-//         setSession(session);
-//         setUser(session?.user ?? false);
-//       }
-//     );
-
-//     return () => {
-//       authListener.unsubscribe();
-//     };
-//   }, []);
-
-//   const value = {
-//     session,
-//     user,
-//   };
-//   return <UserContext.Provider value={value} {...props} />;
-// };
 
 export const useUser = () => {
   const context = useContext(authContext);
@@ -72,7 +29,7 @@ const AuthUser = () => {
 
 export default AuthUser;
 
-//* from youtube tutorial:
+//* below is from youtube tutorial: https://github.com/dom-the-dev/movie-tinder-YouTube/blob/bucket-storage/src/auth.js
 
 const authContext = createContext();
 
