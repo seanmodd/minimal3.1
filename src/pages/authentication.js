5f710d7474a19a49616618d7e0fb011d4a10a107
Sean <>
2022-03-10 13:59:30 -0800

    playing with styled components

---
+++ src/pages/authentication.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+
+const CommonButton = ({ children, color, disabled, size, sx, variant }) => (
+  <Button
+    color={color}
+    disabled={disabled}
+    size={size}
+    sx={sx}
+    variant={variant}
+  >
+    {children}
+  </Button>
+);
+
+const Authentication = () => {
+  const buttonStyles = {
+    fontSize: '0.875rem',
+    fontWeight: 600,
+    textTransform: 'capitalize',
+    color: '#fff',
+    borderRadius: 2.5,
+    '&.MuiButton-contained': {
+      backgroundColor: '#009be5',
+      '&:hover': {
+        backgroundColor: '#006db3',
+        color: '#fff',
+      },
+    },
+    '&.MuiButton-outlined': {
+      color: '#fff',
+      borderColor: '#fff',
+      '&:hover': {
+        backgroundColor: '#000',
+      },
+    },
+  };
+  const gridStyles = {
+    marginTop: '2rem',
+    backgroundColor: '#ebebeb',
+    borderRadius: '0.5rem',
+    padding: '1rem',
+    // boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+    transition: 'all 0.3s ease-in-out',
+    // boxShadow: '0.3s cubic-bezier(0.25, 0.8, 0.25, 1)',
+    // transform: '0.3s cubic-bezier(0.25, 0.8, 0.25, 1)',
+    // change background on hover:
+    '&:hover': {
+      backgroundColor: '#f5f5f5',
+      transition: 'all 0.3s ease-in-out',
+    },
+  };
+
+  return (
+    <Grid sx={gridStyles} item m={16}>
+      This is authentication page.
+      <CommonButton sx={buttonStyles} variant="contained">
+        Add user
+      </CommonButton>
+      <CommonButton sx={buttonStyles} variant="outlined">
+        Add user
+      </CommonButton>
+    </Grid>
+  );
+};
+
+export default Authentication;
